57d0aa3462fe437a1d689524d3ae611d038458b9
Tyler Briles <tyler@mrbri.com>
2016-03-19 09:50:54 -0700

    A web server

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"net/http"
+)
+
 func main() {
 	println("Hello!")
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
 }
